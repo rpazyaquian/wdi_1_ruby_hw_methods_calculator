539533f4eb3c9ae0fd77a7f26f89662b176a7cb8
Rebecca Paz <r.pazyaquian@gmail.com>
2014-09-23 18:27:18 -0400

    initial commit, remember to commit after each passing test

--- calc.rb
+++ calc.rb
@@ -1,1 +1,97 @@
 # Your code goes here
+
+class Calculator
+
+  def self.add(x, y)
+    x + y
+  end
+
+  def self.subtract(x, y)
+    x - y
+  end
+
+  def self.multiply(x, y)
+    x * y
+  end
+
+  def self.divide(x, y)
+    x / y
+  end
+
+  def self.exponent(x, y)
+    x**y
+  end
+
+  def self.square(x)
+    x**2
+  end
+
+  def self.cube(x)
+    x**3
+  end
+
+  def self.root(x)
+    Math.sqrt(x)
+  end
+
+end
+
+def get_number
+  gets.chomp.to_f
+end
+
+def menu
+  options = [
+    '(a)dd',
+    '(s)ubtract',
+    '(m)ultiply',
+    '(d)ivide',
+    '(e)xponent',
+    's(q)uare',
+    '(c)ube',
+    '(r)oot'
+  ]
+  puts "hello, please choose an option:\n#{options.join(", ")}"
+  operation_choice = gets.chomp
+  if operation_choice == 'a'
+    puts "Add:"
+    x = get_number
+    puts "to:"
+    y = get_number
+    puts "#{x} plus #{y} is #{Calculator.add(x, y)}"
+  elsif operation_choice == 's'
+    puts "Subtract:"
+    y = get_number
+    puts "from:"
+    x = get_number
+    puts "#{x} minus #{y} is #{Calculator.subtract(x, y)}"
+  elsif operation_choice == 'm'
+    puts "Multiply:"
+    x = get_number
+    puts "by:"
+    y = get_number
+    puts "#{x} times #{y} is #{Calculator.multiply(x, y)}"
+  elsif operation_choice == 'd'
+    puts "Divide:"
+    x = get_number
+    puts "by:"
+    y = get_number
+    puts "#{x} divided by #{y} is #{Calculator.divide(x, y)}"
+  elsif operation_choice == 'e'
+    puts "Raise:"
+    x = get_number
+    puts "to the power of:"
+    y = get_number
+    puts "#{x} to the power of #{y} is #{Calculator.exponent(x, y)}"
+  elsif operation_choice == 'q'
+    puts "Not implemented yet."
+  elsif operation_choice == 'c'
+    puts "Not implemented yet."
+  elsif operation_choice == 'r'
+    puts "Not implemented yet."
+  else
+    puts "Sorry, that's not a valid option."
+  end
+end
+
+menu
---
+++ calc_spec.rb
@@ -0,0 +1,55 @@
+# yes, this is exactly what you think it is.
+
+require_relative 'calc'
+
+describe Calculator do
+
+  describe "#add" do
+    it "should add the numbers entered" do
+      expect(3).to eq Calculator.add(1, 2)
+    end
+  end
+
+  describe "#subtract" do
+    it "should subtract the numbers entered" do
+      expect(1).to eq Calculator.subtract(3, 2)
+    end
+  end
+
+  describe "#multiply" do
+    it "should multiply the numbers entered" do
+      expect(4).to eq Calculator.multiply(2, 2)
+    end
+  end
+
+  describe "#divide" do
+    it "should divide the numbers entered" do
+      expect(3).to eq Calculator.divide(6, 2)
+    end
+  end
+
+  describe "#exponent" do
+    it "should exponentiate(?) the numbers entered" do
+      expect(9).to eq Calculator.exponent(3, 2)
+    end
+  end
+
+  describe "#square" do
+    it "should square the number entered" do
+      expect(16).to eq Calculator.square(4)
+    end
+  end
+
+  describe "#cube" do
+    it "should cube the number entered" do
+      expect(8).to eq Calculator.cube(2)
+    end
+  end
+
+  describe "#root" do
+    it "should find the square root of the number entered" do
+      expect(3).to eq Calculator.root(9)
+    end
+  end
+
+end
